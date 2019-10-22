0390209911e78c93c8860f5091e935112bb3c59e
s20181102934 <s20181102934@admindeiMac-38.local>
2019-10-22 16:39:00 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  blip
+//
+//  Created by s20181102934 on 2019/10/22.
+//  Copyright © 2019 win. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
